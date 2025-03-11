e4a5e93fba253dafc917531696189874e0fb72aa
Heitor Polidoro <heitor.polidoro@gmail.com>
2025-03-11 18:40:05 -0300

    Refactor baseUrl construction logic for flexibility
    Removed default API port value to allow better configuration control. Updated baseUrl to conditionally include the port only when it is defined. This ensures more robust and environment-flexible URL generation.

--- src/config.js
+++ src/config.js
@@ -1,9 +1,13 @@
 // config.js
 const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost'; // Default value if not set
-export const apiPort = process.env.REACT_APP_API_PORT || 3000; // Default value if not set
+export const apiPort = process.env.REACT_APP_API_PORT; // Default value if not set
 export const allowedUserSub = process.env.REACT_APP_ALLOWED_USER_SUB;
 export const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
-export const baseUrl = `${apiUrl}:${apiPort}`;
+let finalUrl = apiUrl
+if (apiPort !== null) {
+    finalUrl = `${apiUrl}:${apiPort}`;
+}
+export const baseUrl = finalUrl;
 
 
